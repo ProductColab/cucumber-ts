7da19a3f75ef05f14a250722f2bdef9db84ddd32
Nic Galluzzo <nic@galluzzo.us>
2025-03-17 16:48:19 -0700

    cucumber steps

---
+++ features/step_definitions/stepdefs.ts
@@ -0,0 +1,11 @@
+import { When, Then } from '@cucumber/cucumber';
+
+When('I visit the hello world page', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return true;
+});
+
+Then('I see {string}', function (_string) {
+  // Write code here that turns the phrase above into concrete actions
+  return true;
+});
