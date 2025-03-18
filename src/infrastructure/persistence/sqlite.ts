dac92488226a1021e8a86ce532645f8c6f234043
Nic Galluzzo <nic@galluzzo.us>
2025-03-17 19:45:45 -0700

    Refactor database URL configuration in drizzle.config.ts and sqlite.ts to provide a default value for DB_FILE_NAME

--- src/infrastructure/persistence/sqlite.ts
+++ src/infrastructure/persistence/sqlite.ts
@@ -1,7 +1,7 @@
 
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 
-const db = drizzle(process.env.DB_FILE_NAME!);
+const db = drizzle(process.env.DB_FILE_NAME || "./db.sqlite");
 
 export default db;
 export * from 'bun:sqlite';
