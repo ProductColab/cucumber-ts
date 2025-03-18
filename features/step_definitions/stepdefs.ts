e8e0c58f7e3f4bdd4b67725c5aecbe83a8b3bc17
Nic Galluzzo <nic@galluzzo.us>
2025-03-17 18:05:31 -0700

    Add hello function and update cucumber step to log greeting

--- features/step_definitions/stepdefs.ts
+++ features/step_definitions/stepdefs.ts
@@ -1,8 +1,9 @@
 import { When, Then } from '@cucumber/cucumber';
+import { hello } from '@/hello';
 
 When('I visit the hello world page', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return true;
+  const result = hello();
+  console.log(result);
 });
 
 Then('I see {string}', function (_string) {
